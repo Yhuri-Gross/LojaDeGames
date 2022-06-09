cd8c0feb26a534551776a6777ecc8f710cc28617
Yhuri Gross <yhurigross@gmail.com>
2022-06-09 16:14:44 -0300

    Implementando security

--- src/main/java/com/generation/games/model/Produtos.java
+++ src/main/java/com/generation/games/model/Produtos.java
@@ -38,6 +38,18 @@ public class Produtos {
 	@ManyToOne
 	@JsonIgnoreProperties("produtos")
 	private Categorias categorias;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public Long getId() {
 		return id;
